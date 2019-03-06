d20d4637379f0b6d15d51e053e06f61406e9d0cb
Raphaël RIGNIER <r.rignier@leschartreux.net>
2019-03-06 10:50:21 +0100

    declaration phpcas

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,4 +1,14 @@
-<?php require_once 'phpCAS-1.3.6/CAS.php';?>
+<?php require_once 'phpCAS-1.3.6/CAS.php';
+
+$cas_host='cas.leschartreux.com';
+$cas_port=443;
+$cas_context='/';
+$cas_cert='cas.leschartreux.com.pem';
+
+phpCAS::client(CAS_VERSION_2_0, $cas_host, $cas_port, $cas_context);
+phpCAS::setCasServerCACert($cas_cert);
+
+?>
 <!doctype html>
 <html>
 <head>
@@ -77,6 +87,7 @@ $( document ).ready(function() {
 			event.preventDefault();
 			// hide alerts
 			$("#alertMSG").addClass("hidden");
+			<?php ?>
 			// try to login
 			$.ajax({
 				type: "POST",

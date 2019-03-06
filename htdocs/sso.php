8b62ce4097439e1ec27b78414432572ac868a740
Raphaël RIGNIER <r.rignier@leschartreux.net>
2019-03-06 11:19:56 +0100

    sso simple

--- htdocs/sso.php
+++ htdocs/sso.php
@@ -14,3 +14,15 @@ phpCAS::forceAuthentication();
 
 
 ?>
+<html>
+  <head>
+    <title>phpCAS simple client</title>
+  </head>
+  <body>
+    <h1>Successfull Authentication!</h1>
+    
+    <p>the user's login is <b><?php echo phpCAS::getUser(); ?></b>.</p>
+    <p>phpCAS version is <b><?php echo phpCAS::getVersion(); ?></b>.</p>
+    <p><a href="?logout=">Logout</a></p>
+  </body>
+</html>

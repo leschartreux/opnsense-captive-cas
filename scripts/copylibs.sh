34defdf5015b6b3b2a0e22e063a8490adc41bb87
Raphaël RIGNIER <r.rignier@leschartreux.net>
2019-03-07 16:27:14 +0100

    rename copylibs script

--- scripts/copylibs.sh
+++ scripts/copylibs.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-echo test
-
 CPROOT=/var/captiveportal/zone0
 
 echo $CPROOT
@@ -10,6 +8,15 @@ echo $CPROOT
 mkdir -p $CPROOT/usr/local
 cp /usr/local/bin/php-cgi $CPROOT/usr/local
 echo fin mkdir
+LIBS=`ldd /usr/local/bin/php-cgi | awk '{print $3}'`
+for i in $LIBS
+do
+	  DIR=`dirname $i`
+	  if [ ! -d $CPROOT/$DIR ]; then
+	      mkdir -p $CPROOT$DIR
+	  fi
+	  cp $i $CPROOT$DIR/
+done
 
 #extensions
 for R in `cat required`
